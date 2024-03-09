6e2824b64ee09e606ccaab7d7a1b2ba337cbf34a
hyw666 <1920906857@qq.com>
2024-03-09 07:01:45 +0000

    解决adb启动缺动态库

--- ramdisk/rootfs/overlay/cv181x_musl_riscv64/system/auto.sh
+++ ramdisk/rootfs/overlay/cv181x_musl_riscv64/system/auto.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ${CVI_SHOPTS}
 
-export LD_LIBRARY_PATH="/lib:/lib/3rd:/lib/arm-linux-gnueabihf:/usr/lib:/usr/local/lib:/mnt/system/lib:/mnt/system/usr/lib:/mnt/system/usr/lib/3rd:/mnt/data/lib"
+export LD_LIBRARY_PATH="/lib:/lib/3rd:/lib/arm-linux-gnueabihf:/usr/lib:/usr/local/lib:/mnt/system/lib:/mnt/system/usr/lib:/mnt/system/usr/lib/3rd:/mnt/data/lib:/mnt/system/lib/"
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/mnt/system/usr/bin:/mnt/system/usr/sbin:/mnt/data/bin:/mnt/data/sbin"
 
 
