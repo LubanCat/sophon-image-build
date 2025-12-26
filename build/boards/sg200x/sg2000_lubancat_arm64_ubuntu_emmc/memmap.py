e0a3125918c53264e5612fc9e2b0527e731b1a96
李林海 <zgwinli555@163.com>
2025-12-26 06:23:33 +0000

    update lubancat arm64 linux fb

--- build/boards/sg200x/sg2000_lubancat_arm64_ubuntu_emmc/memmap.py
+++ build/boards/sg200x/sg2000_lubancat_arm64_ubuntu_emmc/memmap.py
@@ -64,7 +64,7 @@ class MemoryMap:
 
     # Boot logo is after the ION buffer
     # Framebuffer uses boot logo's reserved memory
-    BOOTLOGO_SIZE = 1800 * SIZE_1K
+    BOOTLOGO_SIZE = 16 * SIZE_1M
     BOOTLOGO_ADDR = ION_ADDR - BOOTLOGO_SIZE
     FRAMEBUFFER_SIZE = BOOTLOGO_SIZE
     FRAMEBUFFER_ADDR = BOOTLOGO_ADDR
